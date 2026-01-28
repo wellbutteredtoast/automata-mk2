5e719aa74047cc16d6de96d66b6facb3dc926422
wellbutteredtoast <swatsonjones@mun.ca>
2026-01-27 23:10:37 -0330

    Things are progressing...

--- src/automata/__main__.py
+++ src/automata/__main__.py
@@ -1,5 +1,13 @@
-def main() -> None:
-    ...
+from __future__ import annotations
+
+import logging
+import sys
+import os
+
+import discord
+from discord.ext import commands
+
+def main() -> None: ...
 
 if __name__ == "__main__":
     main()
